8592947f90e47b3369177c90c31bc8c7b961a578
Manh Hung <hung1most1@gmail.com>
2020-09-24 14:55:10 +0700

    Initial commit

---
+++ android/app/src/main/java/com/example/onesignal_demo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.onesignal_demo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,265 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+//import OneSignal
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _debugLabelString = "";
+  bool _enableConsentButton = false;
+
+  // CHANGE THIS parameter to true if you want to test GDPR privacy consent
+  bool _requireConsent = true;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    if (!mounted) return;
+
+    OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+    OneSignal.shared.setRequiresUserPrivacyConsent(_requireConsent);
+
+    var settings = {
+      OSiOSSettings.autoPrompt: false,
+      OSiOSSettings.promptBeforeOpeningPushUrl: true
+    };
+
+    OneSignal.shared.setNotificationReceivedHandler((OSNotification notification) {
+      this.setState(() {
+        _debugLabelString =
+        "Received notification: \n${notification.jsonRepresentation().replaceAll("\\n", "\n")}";
+      });
+    });
+
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      this.setState(() {
+        _debugLabelString =
+        "Opened notification: \n${result.notification.jsonRepresentation().replaceAll("\\n", "\n")}";
+      });
+    });
+
+    OneSignal.shared
+        .setInAppMessageClickedHandler((OSInAppMessageAction action) {
+      this.setState(() {
+        _debugLabelString =
+        "In App Message Clicked: \n${action.jsonRepresentation().replaceAll("\\n", "\n")}";
+      });
+    });
+
+    OneSignal.shared
+        .setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+      print("SUBSCRIPTION STATE CHANGED: ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+      print("PERMISSION STATE CHANGED: ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared.setEmailSubscriptionObserver(
+            (OSEmailSubscriptionStateChanges changes) {
+          print("EMAIL SUBSCRIPTION STATE CHANGED ${changes.jsonRepresentation()}");
+        });
+
+    // NOTE: Replace with your own app ID from https://www.onesignal.com
+    await OneSignal.shared
+        .init("f81d681a-6001-489d-96d1-62a485cd4d7a", iOSSettings: settings);
+
+    OneSignal.shared
+        .setInFocusDisplayType(OSNotificationDisplayType.notification);
+
+    bool requiresConsent = await OneSignal.shared.requiresUserPrivacyConsent();
+
+    this.setState(() {
+      _enableConsentButton = requiresConsent;
+    });
+
+    // Some examples of how to use In App Messaging public methods with OneSignal SDK
+    oneSignalInAppMessagingTriggerExamples();
+
+    // Some examples of how to use Outcome Events public methods with OneSignal SDK
+    oneSignalOutcomeEventsExamples();
+  }
+
+  void _handleConsent() {
+    print("Setting consent to true");
+    OneSignal.shared.consentGranted(true);
+
+    print("Setting state");
+    this.setState(() {
+      _enableConsentButton = false;
+    });
+  }
+
+  void _handleSendNotification() async {
+    var status = await OneSignal.shared.getPermissionSubscriptionState();
+    var playerId = status.subscriptionStatus.userId;
+    var imgUrlString =
+        "http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg";
+
+    var notification = OSCreateNotification(
+        playerIds: [playerId],
+        content: "this is a test from OneSignal's Flutter SDK",
+        heading: "Test Notification",
+        iosAttachments: {"id1": imgUrlString},
+        bigPicture: imgUrlString,
+        buttons: [
+          OSActionButton(text: "test1", id: "id1"),
+          OSActionButton(text: "test2", id: "id2")
+        ]);
+
+    var response = await OneSignal.shared.postNotification(notification);
+
+    this.setState(() {
+      _debugLabelString = "Sent notification with response: $response";
+    });
+  }
+
+
+
+  oneSignalInAppMessagingTriggerExamples() async {
+    /// Example addTrigger call for IAM
+    /// This will add 1 trigger so if there are any IAM satisfying it, it
+    /// will be shown to the user
+    OneSignal.shared.addTrigger("trigger_1", "one");
+
+    /// Example addTriggers call for IAM
+    /// This will add 2 triggers so if there are any IAM satisfying these, they
+    /// will be shown to the user
+    Map<String, Object> triggers = new Map<String, Object>();
+    triggers["trigger_2"] = "two";
+    triggers["trigger_3"] = "three";
+    OneSignal.shared.addTriggers(triggers);
+
+    // Removes a trigger by its key so if any future IAM are pulled with
+    // these triggers they will not be shown until the trigger is added back
+    OneSignal.shared.removeTriggerForKey("trigger_2");
+
+    // Get the value for a trigger by its key
+    Object triggerValue = await OneSignal.shared.getTriggerValueForKey("trigger_3");
+    print("'trigger_3' key trigger value: " + triggerValue);
+
+    // Create a list and bulk remove triggers based on keys supplied
+    List<String> keys = new List<String>();
+    keys.add("trigger_1");
+    keys.add("trigger_3");
+    OneSignal.shared.removeTriggersForKeys(keys);
+
+    // Toggle pausing (displaying or not) of IAMs
+    OneSignal.shared.pauseInAppMessages(false);
+  }
+
+  oneSignalOutcomeEventsExamples() async {
+    // Await example for sending outcomes
+    outcomeAwaitExample();
+
+    // Send a normal outcome and get a reply with the name of the outcome
+    OneSignal.shared.sendOutcome("normal_1");
+    OneSignal.shared.sendOutcome("normal_2").then((outcomeEvent) {
+      print(outcomeEvent.jsonRepresentation());
+    });
+
+    // Send a unique outcome and get a reply with the name of the outcome
+    OneSignal.shared.sendUniqueOutcome("unique_1");
+    OneSignal.shared.sendUniqueOutcome("unique_2").then((outcomeEvent) {
+      print(outcomeEvent.jsonRepresentation());
+    });
+
+    // Send an outcome with a value and get a reply with the name of the outcome
+    OneSignal.shared.sendOutcomeWithValue("value_1", 3.2);
+    OneSignal.shared.sendOutcomeWithValue("value_2", 3.9).then((outcomeEvent) {
+      print(outcomeEvent.jsonRepresentation());
+    });
+  }
+
+  Future<void> outcomeAwaitExample() async {
+    var outcomeEvent = await OneSignal.shared.sendOutcome("await_normal_1");
+    print(outcomeEvent.jsonRepresentation());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: const Text('OneSignal Flutter Demo'),
+            backgroundColor: Color.fromARGB(255, 212, 86, 83),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(10.0),
+            child: SingleChildScrollView(
+              child: new Table(
+                children: [
+                  new TableRow(children: [
+                    new OneSignalButton("Provide GDPR Consent", _handleConsent,
+                        _enableConsentButton)
+                  ]),
+                  new TableRow(children: [
+                    new OneSignalButton("Post Notification",
+                        _handleSendNotification, !_enableConsentButton)
+                  ]),
+                  new TableRow(children: [
+                    new Container(
+                      child: new Text(_debugLabelString),
+                      alignment: Alignment.center,
+                    )
+                  ]),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+typedef void OnButtonPressed();
+
+class OneSignalButton extends StatefulWidget {
+  final String title;
+  final OnButtonPressed onPressed;
+  final bool enabled;
+
+  OneSignalButton(this.title, this.onPressed, this.enabled);
+
+  State<StatefulWidget> createState() => new OneSignalButtonState();
+}
+
+class OneSignalButtonState extends State<OneSignalButton> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Table(
+      children: [
+        new TableRow(children: [
+          new FlatButton(
+            disabledColor: Color.fromARGB(180, 212, 86, 83),
+            disabledTextColor: Colors.white,
+            color: Color.fromARGB(255, 212, 86, 83),
+            textColor: Colors.white,
+            padding: EdgeInsets.all(8.0),
+            child: new Text(widget.title),
+            onPressed: widget.enabled ? widget.onPressed : null,
+          )
+        ]),
+        new TableRow(children: [
+          Container(
+            height: 8.0,
+          )
+        ]),
+      ],
+    );
+  }
+}
